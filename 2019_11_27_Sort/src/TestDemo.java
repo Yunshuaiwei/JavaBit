9530dfe48c8476e137262ebc5ed1412cbb8c3ffa
Yunshuaiwei <1779001867@qq.com>
2019-11-27 21:28:18 +0800

    练习

---
+++ 2019_11_27_Sort/src/TestDemo.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+/**
+ * @Auther
+ * @Date 2019/11/27 18:41
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void test(){
+        String str="12334a6";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i <chars.length ; i++) {
+            if(chars[i]>'9'){
+                System.out.println("不在！");
+                return;
+            }
+        }
+        System.out.println("在！");
+    }
+    @Test
+    public String test2(){
+        char [] array={'a','b','c','d'};
+        return new String (array);
+}
+}
