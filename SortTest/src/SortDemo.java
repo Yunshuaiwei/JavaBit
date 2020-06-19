a6f6a309f7cea7fbf29ff1510c90ede8dc333f41
ysw <1779001867@qq.com>
2020-06-19 21:04:23 +0800

    算法题

---
+++ SortTest/src/SortDemo.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/6/19 20:39
+ * @Version
+ **/
+public class SortDemo {
+    public static void main(String[] args) {
+        int[] arr = {6, 8, 2, 1, 0, 3, 5, 4, 7, 9, 10};
+        SortDemo s = new SortDemo();
+//        s.bubbleSort(arr);
+        s.quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void bubbleSort(int[] arr) {
+        boolean flag = true;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = false;
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+            if (flag) {
+                break;
+            } else {
+                flag = true;
+            }
+        }
+    }
+
+    public void quickSort(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(l + r) / 2];
+        while (l < r) {
+            while (arr[l] < pivot) {
+                l++;
+            }
+            while (arr[r] > pivot) {
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            arr[l] ^= arr[r];
+            arr[r] ^= arr[l];
+            arr[l] ^= arr[r];
+            if (arr[l] == pivot) {
+                r--;
+            }
+            if (arr[r] == pivot) {
+                l++;
+            }
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        if (right > l) {
+            quickSort(arr, l, right);
+        }
+    }
+
+
+}
