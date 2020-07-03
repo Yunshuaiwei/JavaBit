5fbd3f419648b9e6eb08f149a80fa894101cf848
ysw <1779001867@qq.com>
2020-07-03 22:22:02 +0800

    算法题

--- SortTest/src/SortDemo.java
+++ SortTest/src/SortDemo.java
@@ -12,7 +12,7 @@ public class SortDemo {
         SortDemo s = new SortDemo();
 //        s.bubbleSort(arr);
 //        s.quickSort(arr, 0, arr.length - 1);
-        s.sortHeap(arr);
+        s.sortHeap2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -205,6 +205,36 @@ public class SortDemo {
      * @Param [arr, i, length]
      **/
     public void adjustHeap(int arr[], int i, int length) {
+        int temp = arr[i];
+        for (int j = i * 2 + 1; j < length; j = j * 2 + 1) {
+            if (j + 1 < length && arr[j] < arr[j + 1]) {
+                j++;
+            }
+            if (arr[j] > temp) {
+                arr[i] = arr[j];
+                i = j;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;
+    }
+
+    public void sortHeap2(int[] arr) {
+        int temp = 0;
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            adjustHeap1(arr, i, arr.length);
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+            adjustHeap1(arr, 0, i);
+
+        }
+    }
+
+    public void adjustHeap1(int[] arr, int i, int length) {
         int temp = arr[i];
         for (int j = i * 2 + 1; j < length; j = j * 2 + 1) {
             if (j + 1 < length && arr[j] < arr[j + 1]) {
